61ca57be6049ddbd4787fcc5f42256b8954b48e9
Theodor Moroianu <theodor.moroianu@gmail.com>
2020-11-10 13:13:49 +0200

    Finish random population

--- dataset_parser.py
+++ dataset_parser.py
@@ -2,21 +2,26 @@ import csv
 import random
 
 # Constants used for creating the DB
-LOCATIE_COUNT = 2000
-FURNIZOR_COUNT, MAGAZIN_COUNT = [100, 100]
+LOCATIE_COUNT = 200
+FURNIZOR_COUNT, MAGAZIN_COUNT = [20, 10]
 CUMPARATOR_COUNT = 1000
-ANGAJAT_COUNT = 500
-PRODUS_COUNT = 3000
+ANGAJAT_COUNT = 100
+PRODUS_COUNT = 500
 VANZARE_COUNT = 3000
 CONTINUT_VANZARE_COUNT = 5000
-REVIEW_COUNT = 1000
+REVIEW_COUNT = 100
+CONTINUT_FURNIZARE_COUNT = 1000
+FURNIZARE_COUNT = 100
+DISPONIBILITATE_FURNIZOR_COUNT = 300
+DISPONIBILITATE_MAGAZIN_COUNT = 1000
+
 
 # citiy -> 1, country -> 4, ...
 cities = None
 # name -> 0
 first_names, last_names = None, None
 # product -> 3, description -> 10
-products = None
+products, cost_produts = None, None
 # company -> 0, website -> 1
 companies = None
 # rating -> 0, review -> 1
@@ -94,6 +99,8 @@ def CreateProdus():
         fout.write("INSERT INTO produs VALUES(" +
                     str(i) + ", '" +
                     nume + "', NULL);\n")
+    global cost_produts
+    cost_produts = [random.randint(100, 1000000) for _ in range(PRODUS_COUNT)]
 
 
 def CreateFurnisor():
@@ -110,15 +117,165 @@ def CreateFurnisor():
                     telefon + "', " +
                     str(locatieID) + ");\n")
     
+def CreateAngajatMagazin():
+    fout.write("\n\n -- Populating table Angajat")
+    fout.write(" -- Disable fk\n")
+    fout.write("ALTER TABLE angajat\n    DISABLE CONSTRAINT angajatfkmagazin;\n\n")
 
+    ids = [i for i in range(ANGAJAT_COUNT)]
+    random.shuffle(ids)
+
+    for i in range(ANGAJAT_COUNT):
+        nume = random.choice(last_names)[0]
+        prenume = random.choice(first_names)[0]
+        salariu = 10000 - random.randint(1, 10 * i + 1)
+        DataAngajare = GetDate()
+        magazinid = "NULL" if i == 0 else str(i - 1)
+        manager = "NULL" if i == 0 else str(ids[0])
+
+        if i > MAGAZIN_COUNT:
+            m = random.randint(1, MAGAZIN_COUNT)
+            manager = str(ids[m])
+            magazinid = str(m - 1)
+        
+        fout.write("INSERT INTO angajat VALUES(" +
+                    str(ids[i]) + ", '" +
+                    nume + "', '" +
+                    prenume + "', " +
+                    magazinid + ", " + 
+                    manager + ", " +
+                    str(salariu) + ", " +
+                    DataAngajare + ");\n")
+
+    fout.write("\n\n -- Populating Magazin\n")
+
+    for i in range(MAGAZIN_COUNT):    
+        fout.write("INSERT INTO magazin VALUES(" +
+                    str(i) + ", " +
+                    str(random.randint(0, LOCATIE_COUNT - 1)) + ", " +
+                    str(ids[i + 1]) + ", " +
+                    GetDate() + ");\n")
+
+    fout.write("\nALTER TABLE angajat\n    ENABLE CONSTRAINT angajatfkmagazin;\n")
+
+def CreateFurnizare():
+    furnizare = [1 for i in range(FURNIZARE_COUNT)]
+    for i in range(FURNIZARE_COUNT, CONTINUT_FURNIZARE_COUNT):
+        furnizare[random.randint(0, FURNIZARE_COUNT - 1)] += 1
+
+    fout.write("\n\nPopulating furnizare, continut_furnizare and disponibilitate_furnizor\n\n")
+    
+    for i in range(FURNIZARE_COUNT):
+        continut = [i for i in range(PRODUS_COUNT)]
+        random.shuffle(continut)
+        continut = continut[0:furnizare[i]]
+        cost = [cost_produts[i] + random.randint(-50, 50) for i in continut]
+        cantitate = [random.randint(1, 10) for i in cost]
+
+        suma = 0
+        for j in range(furnizare[i]):
+            suma += cost[j] * cantitate[j]
+
+        fout.write("INSERT INTO furnizare VALUES(" +
+                    str(i) + ", " +
+                    str(random.randint(0, MAGAZIN_COUNT - 1)) + ", " +
+                    str(suma) + ", " +
+                    str(random.randint(0, FURNIZOR_COUNT)) + ");\n")
+        
+        for j in range(furnizare[i]):
+            fout.write("INSERT INTO continutfurnizare VALUES(" +
+                        str(i) + ", " +
+                        str(continut[j]) + ", " +
+                        str(cantitate[j]) + ", " +
+                        str(cost[j]) + ");\n")
+
+    added = {}
+    for i in range(DISPONIBILITATE_FURNIZOR_COUNT):
+        while True:
+            f = random.randint(0, FURNIZOR_COUNT - 1)
+            p = random.randint(0, PRODUS_COUNT - 1)
+            if (f, p) in added:
+                continue
+
+            added[(f, p)] = None
+            fout.write("INSERT INTO disponibilitatefurnizor VALUES(" +
+                        str(f) + ", " + str(p) + ", " +
+                        str(random.randint(1, 1000)) + ", " +
+                        str(random.randint(-50, 50) + cost_produts[p]) +
+                        ");\n")
+            break
+
+
+def CreateDisponibilitateMagazin():
+    added = {}
+    for i in range(DISPONIBILITATE_MAGAZIN_COUNT):
+        while True:
+            f = random.randint(0, MAGAZIN_COUNT - 1)
+            p = random.randint(0, PRODUS_COUNT - 1)
+            if (f, p) in added:
+                continue
+
+            added[(f, p)] = None
+
+            fout.write("INSERT INTO disponibilitatefurnizor VALUES(" +
+                        str(p) + ", " + str(f) + ", " +
+                        str(random.randint(1, 1000)) + ", " +
+                        str(random.randint(-50, 50) + cost_produts[p]) +
+                        ");\n")
+            break
+
+def CreateVanzare():
+    vanzare = [1 for i in range(VANZARE_COUNT)]
+    for i in range(VANZARE_COUNT, CONTINUT_VANZARE_COUNT):
+        vanzare[random.randint(0, VANZARE_COUNT - 1)] += 1
+
+    for i in range(VANZARE_COUNT):
+        continut = [i for i in range(PRODUS_COUNT)]
+        random.shuffle(continut)
+        continut = continut[0:vanzare[i]]
+        cost = [cost_produts[i] + random.randint(-50, 50) for i in continut]
+        cantitate = [random.randint(1, 10) for i in cost]
+
+        suma = 0
+        for j in range(vanzare[i]):
+            suma += cost[j] * cantitate[j]
+
+        fout.write("INSERT INTO vanzare VALUES(" +
+                    str(i) + ", " +
+                    str(random.randint(0, MAGAZIN_COUNT - 1)) + ", " +
+                    str(random.randint(0, CUMPARATOR_COUNT)) + ", " +
+                    GetDate() + ", " + 
+                    str(suma) + ");\n")
+        
+        for j in range(vanzare[i]):
+            fout.write("INSERT INTO continutvanzare VALUES(" +
+                        str(i) + ", " +
+                        str(continut[j]) + ", " +
+                        str(cost[j]) + ", " +
+                        str(cantitate[j]) + ");\n")
+            if (random.randint(1, 7) == 1):
+                rev = random.choice(reviews)
+                
+                fout.write("INSERT INTO review VALUES(" +
+                            str(i) + ", " +
+                            str(continut[j]) + ", " +
+                            rev[0] + ", '" +
+                            rev[1] + "');\n")
+                            
 def main():
     # Parse data from csvs
     ParseData()
 
-    # CreateLocatie()
-    # CreateCumparator()
-    # CreateProdus()
+    CreateLocatie()
+    CreateCumparator()
+    CreateProdus()
     CreateFurnisor()
+    CreateAngajatMagazin()
+    CreateFurnizare()
+    CreateVanzare()
+    CreateDisponibilitateMagazin()
+
+    fout.write("\nCOMMIT;\n")
 
 if __name__ == "__main__":
     main()
